0159366c130493a94de4919a0a6da2c95f0b1c65
Klayna-W <132109068+Klayna-W@users.noreply.github.com>
2025-08-22 13:59:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ if "comments" not in st.session_state:
 # -----------------------------
 # --- Load Data ---
 # -----------------------------
+daily_metrics = pd.read_csv("data.csv", parse_dates=["Date"])
 employees = pd.read_csv("Employees.csv", parse_dates=["Hire Date"])
 LAE_Metrics = pd.read_csv("NewData.csv", parse_dates=["Date"])
 Hours = pd.read_csv("Hours.csv", parse_dates=["Date"])
